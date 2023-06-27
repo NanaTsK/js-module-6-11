d645b45a995f7c19136ebb3fce04f84c04386651
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-06-28 00:52:25 +0200

    mod-6

---
+++ js/js-module6.js
@@ -0,0 +1,52 @@
+// const listWithId = document.querySelector('#menu');
+// listWithId.style.textTransform = 'uppercase';
+// listWithId.style.fontSize = '24px';
+// console.log(listWithId);
+
+// const listWithClass = document.querySelector('.menu');
+// console.log(listWithClass);
+
+// const menuItemsByTagName = document.querySelectorAll("li");
+// console.log(menuItemsByTagName);
+
+// const menuItemsByClass = document.querySelectorAll(".menu-item");
+// console.log(menuItemsByClass);
+
+// const firstMenuItem = document.querySelector(".menu-item");
+// firstMenuItem.style.color = 'tomato';
+// console.log(firstMenuItem);
+
+
+//* _________________________________________________________ *//
+
+//*     ----->     ----->     Example 1 - Властивості та атрибути
+//===================================================
+// const message = document.querySelector("#message");
+// console.log(message.value); // Default textarea message
+
+// //===================================================
+// const activeLink = document.querySelector(".btn.active");
+// console.log(activeLink.href); // https://s.codepen.io/about
+
+// //===================================================
+// const image = document.querySelector(".image");
+// console.log(image.src); // https://placeimg.com/640/480/animals
+// image.src = 'https://placeimg.com/640/480/tech';
+
+//* _________________________________________________________ *//
+
+//*     ----->     ----->     Example 2 - Властивість textContent
+//===================================================
+// const title = document.querySelector(".article-title");
+// title.textContent = "Welcome to heaven";
+// console.log(title.textContent);
+
+// const text = document.querySelector(".article-text");
+// console.log(text.textContent);
+
+// //* _________________________________________________________ *//
+
+//*     ----->     ----->     Example 3 - Властивість classList
+//===================================================
+
+
