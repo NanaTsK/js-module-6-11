769aecc96a7b8aa3558a8ab99dac30a2087a53a2
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-10 10:16:46 +0300

    mod-7

--- js/js-module7.js
+++ js/js-module7.js
@@ -1,4 +1,8 @@
 
+//*     ----->     ----->     Example 1                                
+//===================================================
+console.log(`____________________`);
+console.log(`Example 1 :`);
 
 const parent = document.querySelector("#parent");
 const child = document.querySelector("#child");
@@ -16,5 +20,22 @@ descendant.addEventListener("click", () => {
   alert("Descendant click handler");
 });
 
-
 console.log(`wow`);
+
+//*     ----->     ----->     Example 2                                
+//===================================================
+console.log(`____________________`);
+console.log(`Example 2 :`);
+
+const simpleBox = document.querySelector(".simple-box");
+simpleBox.addEventListener("mousemove", function (evt) { 
+  simpleBox.innerHTML =
+    `clientX - ${evt.clientX} <br> clientY - ${evt.clientY}`
+    console.log(evt.clientX);
+    console.log(evt.clientY);
+});
+
+//*     ----->     ----->     Example xx                                
+//===================================================
+console.log(`____________________`);
+console.log(`Example xx :`);
