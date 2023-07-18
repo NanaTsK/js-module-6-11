a80bd7e0ed77eb879a969f2d02a78dacf802a0d6
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-18 10:37:07 +0200

    mod-9

--- js/js-module9.js
+++ js/js-module9.js
@@ -13,3 +13,52 @@ setTimeout(() => {
 
 // Will run second
 console.log("Third log");
+
+// *     ----->     ----->     Example 2                                
+// ===================================================
+console.log(`____________________`);
+console.log(`Example 2 :`);
+
+const button = document.querySelector("button");
+
+const onClick = () => {
+  setTimeout(() => {
+    alert("I love async JS!");
+  }, 2000);
+};
+
+// Two seconds after clicking the button, 
+// alert specified inside the setTimeout callback will appear.
+button.addEventListener("click", onClick);
+
+
+// *     ----->     ----->     Example 3                                
+// ===================================================
+console.log(`____________________`);
+console.log(`Example 3 :`);
+
+const startBtn = document.querySelector(".js-start");
+const stopBtn = document.querySelector(".js-stop");
+let timerId = null;
+
+startBtn.addEventListener("click", () => { 
+    timerId = setInterval(() => {
+    console.log("I love async JS! ${Math.random()}");
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    console.log("Interval with id ${timerId} has stopped!")
+});
+
+
+// *     ----->     ----->     Example 2                                
+// ===================================================
+console.log(`____________________`);
+console.log(`Example 2 :`);
+
+// *     ----->     ----->     Example 2                                
+// ===================================================
+console.log(`____________________`);
+console.log(`Example 2 :`);
