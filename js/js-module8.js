f974bdea5ec89bd4dcd702bcb43e2b2937e225ca
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-22 18:11:09 +0200

    mod-8

--- js/js-module8.js
+++ js/js-module8.js
@@ -1,3 +1,6 @@
+// 
+import BSN from "//cdn.jsdelivr.net/npm/bootstrap.native@3.0.5/dist/bootstrap-native.esm.min.js";
+
 // *     ----->     ----->     Example 1                                
 // ===================================================
 console.log(`____________________`);
@@ -46,6 +49,110 @@ sessionStorage.setItem("session", "test session")
 
 // *     ----->     ----->     Example 3                                
 // ===================================================
-// console.log(`____________________`);
-// console.log(`Example 3 :`);
+console.log(`____________________`);
+console.log(`Example 3 :`);
 
+try {
+    console.log(tet - value);
+} catch (err) {
+    console.log(err);
+} finally { 
+    // something to do anyways - remove proloader or close modal window
+}
+
+//* Nullish coalescing operator (??)
+const foo = null ?? 'default string';
+console.log(foo);
+// Expected output: "default string"
+
+const baz = 0 ?? 42;
+console.log(baz);
+// Expected output: 0
+
+// *     ----->     ----->     Example 4  -> setTimeout                               
+// ===================================================
+console.log(`____________________`);
+console.log(`Example 4 :`);
+
+const NOTIFICATION_DELAY = 3000;
+let timeoutId = null;
+
+const notification = document.querySelector(".js-alert");
+notification.addEventListener("click", onNotificationClick);
+
+showNotification();
+
+function onNotificationClick() { 
+    hideNotification();
+    clearTimeout(timeoutId);
+};
+function showNotification() { 
+    notification.classList.add("is-visible");
+    timeoutId = setTimeout(() => {
+        //console.log("Alert closed automatically");
+        hideNotification();
+    }, NOTIFICATION_DELAY);
+};
+
+function hideNotification() { 
+    notification.classList.remove("is-visible");
+};
+
+// *     ----->     ----->     Example 5   - надоедалка                  
+// ===================================================
+console.log(`____________________`);
+console.log(`Example 5 :`);
+
+//* import BSN from 'bootstrap.native';
+
+const refs = {
+    modal: document.querySelector("#subscription-modal"),
+    subscribeBtn: document.querySelector("button[data-subscribe]"),
+};
+const modal = new BSN.Modal("#subscription-modal");
+const PROMPT_DELAY = 3000;
+const MAX_PROMPT_ATTEMPTS = 3;
+let promptCounter = 0;
+let hasSubscribed = false;
+
+openModal();
+
+refs.modal.addEventListener("hide.bs.modal", () => { 
+    console.log("catch");
+    openModal();
+});
+
+refs.subscribeBtn.addEventListener("click", () => { 
+    hasSubscribed = true;
+    modal.hide();
+
+});
+
+function openModal() { 
+        if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscribed ) { 
+        console.log("Modal stopped - max attempts reached or subscribed");
+        return;
+        }
+    setTimeout(() => {
+    console.log("Open spammy");
+        modal.show();
+        promptCounter += 1;
+}, PROMPT_DELAY);
+
+};
+
+
+
+
+// let promptCounter = 0;
+// let hasSubscribed = false;
+
+// const intervalId = setInterval(() => {
+    // if (promptCounter === MAX_PROMPT_ATTEMPTS) { 
+    //     console.log("Interval stopped automatically");
+    //     clearInterval(intervalId);
+    //     return;
+    // }
+//     console.log("Please Subscribe! -" + Date.now());
+//     promptCounter += 1;
+// }, PROMPT_DELAY);
