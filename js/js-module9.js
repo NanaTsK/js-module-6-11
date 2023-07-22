286e78832a02bada6ef74dbc8bf88b73fbb823b8
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-22 18:22:54 +0200

    mod-9

--- js/js-module9.js
+++ js/js-module9.js
@@ -1,3 +1,6 @@
+
+import BSN from "//cdn.jsdelivr.net/npm/bootstrap.native@3.0.5/dist/bootstrap-native.esm.min.js";
+
 // *     ----->     ----->     Example 1                                
 // ===================================================
 console.log(`____________________`);
@@ -53,15 +56,15 @@ stopBtn.addEventListener("click", () => {
 });
 
 
-// *     ----->     ----->     Example 2                                
+// *     ----->     ----->     Example 4                                
 // ===================================================
 console.log(`____________________`);
-console.log(`Example 2 :`);
+console.log(`Example 4 :`);
 
-// *     ----->     ----->     Example 2                                
+// *     ----->     ----->     Example 5                                
 // ===================================================
 console.log(`____________________`);
-console.log(`Example 2 :`);
+console.log(`Example 5 :`);
 
 const date = new Date();
 
@@ -72,3 +75,75 @@ console.log(date);
 date.setFullYear(2040, 4, 8);
 console.log(date);
 // "Tue May 08 2040 14:50:00 GMT+0300"
+
+// *     ----->     ----->     Example 6  -> setTimeout                               
+// ===================================================
+console.log(`____________________`);
+console.log(`Example 6 :`);
+
+const NOTIFICATION_DELAY = 3000;
+let timeoutId = null;
+
+const notification = document.querySelector(".js-alert");
+notification.addEventListener("click", onNotificationClick);
+
+showNotification();
+
+function onNotificationClick() { 
+    hideNotification();
+    clearTimeout(timeoutId);
+};
+function showNotification() { 
+    notification.classList.add("is-visible");
+    timeoutId = setTimeout(() => {
+        //console.log("Alert closed automatically");
+        hideNotification();
+    }, NOTIFICATION_DELAY);
+};
+
+function hideNotification() { 
+    notification.classList.remove("is-visible");
+};
+
+// *     ----->     ----->     Example 7   - надоедалка                  
+// ===================================================
+console.log(`____________________`);
+console.log(`Example 7 :`);
+
+//* import BSN from 'bootstrap.native';
+
+const refs = {
+    modal: document.querySelector("#subscription-modal"),
+    subscribeBtn: document.querySelector("button[data-subscribe]"),
+};
+const modal = new BSN.Modal("#subscription-modal");
+const PROMPT_DELAY = 3000;
+const MAX_PROMPT_ATTEMPTS = 3;
+let promptCounter = 0;
+let hasSubscribed = false;
+
+openModal();
+
+refs.modal.addEventListener("hide.bs.modal", () => { 
+    console.log("catch");
+    openModal();
+});
+
+refs.subscribeBtn.addEventListener("click", () => { 
+    hasSubscribed = true;
+    modal.hide();
+
+});
+
+function openModal() { 
+        if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscribed ) { 
+        console.log("Modal stopped - max attempts reached or subscribed");
+        return;
+        }
+    setTimeout(() => {
+    console.log("Open spammy");
+        modal.show();
+        promptCounter += 1;
+}, PROMPT_DELAY);
+
+};
