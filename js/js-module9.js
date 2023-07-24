d034f4d5aaee4a8597cb3e45a4d3b8f81cbd3aa8
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-24 16:39:00 +0200

    mod-9

--- js/js-module9.js
+++ js/js-module9.js
@@ -173,39 +173,39 @@ console.log("date1", date1);
 console.log(`____________________`);
 console.log(`Example 9 :`);
 
-const timer = {
+// const timer = {
 
-  start() { 
-    const startTime = Date.now();
+//   start() { 
+//     const startTime = Date.now();
 
-    setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = currentTime - startTime;
-      const {hours, mins, secs} = getTimeComponents(deltaTime);
+//     setInterval(() => {
+//       const currentTime = Date.now();
+//       const deltaTime = currentTime - startTime;
+//       const {hours, mins, secs} = getTimeComponents(deltaTime);
         
-      console.log(`${hours}:${mins}:${secs}`);
+//       console.log(`${hours}:${mins}:${secs}`);
 
-      // console.log("start -> startTime", startTime);
-      // console.log(currentTime - startTime);
+//       // console.log("start -> startTime", startTime);
+//       // console.log(currentTime - startTime);
 
     
-    }, 1000);
-  }
-};
-timer.start();
+//     }, 1000);
+//   }
+// };
+// timer.start();
 
-function pad(value) { 
-  return String(value).padStart(2, "0");
-};
+// function pad(value) { 
+//   return String(value).padStart(2, "0");
+// };
 
-function getTimeComponents(time) {
-    const hours = 
-      pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+// function getTimeComponents(time) {
+//     const hours = 
+//       pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+//     const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//     const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
-    return { hours, mins, secs };
-  }
+//     return { hours, mins, secs };
+//   }
 
 // *     ----->     ----->     Example 10                     
 // ===================================================
